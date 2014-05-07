4df7764f4df4e3f0bc0edb45376b249869040742
Derek Adams <derek.adams@reveal-tech.com>
2014-05-07 15:26:31 -0400

    Added example of using a protobuf custom command implementation.

--- SiteWhereExample/src/main/java/com/sitewhere/android/example/SiteWhereExample.java
+++ SiteWhereExample/src/main/java/com/sitewhere/android/example/SiteWhereExample.java
@@ -15,8 +15,12 @@
  */
 package com.sitewhere.android.example;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
 import android.app.Fragment;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -24,13 +28,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.sitewhere.android.generated.Android;
+import com.sitewhere.android.generated.Android.AndroidSpecification._Header;
+import com.sitewhere.android.generated.Android.AndroidSpecification.changeBackground;
 import com.sitewhere.android.messaging.SiteWhereMessagingException;
 import com.sitewhere.android.mqtt.MqttConfiguration;
 import com.sitewhere.android.mqtt.MqttService;
 import com.sitewhere.android.protobuf.SiteWhereProtobufActivity;
 import com.sitewhere.device.provisioning.protobuf.proto.Sitewhere.Device.Header;
 import com.sitewhere.device.provisioning.protobuf.proto.Sitewhere.Device.RegistrationAck;
-import com.sitewhere.device.provisioning.protobuf.proto.Sitewhere.Device.RegistrationAckState;
 
 /**
  * SiteWhere sample activity.
@@ -125,7 +131,7 @@ public class SiteWhereExample extends SiteWhereProtobufActivity {
 	protected void onConnectedToSiteWhere() {
 		Log.d(TAG, "Connected to SiteWhere.");
 		try {
-			registerDevice(getUniqueDeviceId(), "7dfd6d63-5e8d-4380-be04-fc5c73801dfb", null);
+			registerDevice(getUniqueDeviceId(), "d2604433-e4eb-419b-97c7-88efe9b2cd41", null);
 		} catch (SiteWhereMessagingException e) {
 			Log.e(TAG, "Unable to send device registration to SiteWhere.", e);
 		}
@@ -157,21 +163,6 @@ public class SiteWhereExample extends SiteWhereProtobufActivity {
 			break;
 		}
 		}
-		if (ack.getState() != RegistrationAckState.REGISTRATION_ERROR) {
-			sendData();
-		}
-	}
-
-	/**
-	 * Once registered, send measurement.
-	 */
-	public void sendData() {
-		try {
-			sendMeasurement(getUniqueDeviceId(), null, "engine.temp", 125.0);
-			Log.d(TAG, "Sent measurement.");
-		} catch (SiteWhereMessagingException e) {
-			Log.e(TAG, "Error sending measurement.", e);
-		}
 	}
 
 	/*
@@ -192,6 +183,42 @@ public class SiteWhereExample extends SiteWhereProtobufActivity {
 	@Override
 	protected void onReceivedCustomCommand(byte[] payload) {
 		Log.d(TAG, "Received custom command.");
+		ByteArrayInputStream stream = new ByteArrayInputStream(payload);
+		try {
+			_Header header = Android.AndroidSpecification._Header.parseDelimitedFrom(stream);
+			switch (header.getCommand()) {
+			case CHANGEBACKGROUND: {
+				final changeBackground cb = changeBackground.parseDelimitedFrom(stream);
+				runOnUiThread(new Runnable() {
+
+					@Override
+					public void run() {
+						getWindow().getDecorView().setBackgroundColor(Color.parseColor(cb.getColor()));
+					}
+				});
+				sendAck(getUniqueDeviceId(), header.getOriginator(), "Updated background color.");
+				Log.i(TAG, "Sent reponse to 'changeBackground' command.");
+				break;
+			}
+			case PING: {
+				sendAck(getUniqueDeviceId(), header.getOriginator(), "Acknowledged.");
+				Log.i(TAG, "Sent reponse to 'ping' command.");
+				break;
+			}
+			case TESTEVENTS: {
+				sendMeasurement(getUniqueDeviceId(), header.getOriginator(), "engine.temp", 170.0);
+				sendLocation(getUniqueDeviceId(), header.getOriginator(), 33.7550, -84.3900, 0.0);
+				sendAlert(getUniqueDeviceId(), header.getOriginator(), "engine.overheat",
+						"Engine is overheating!");
+				Log.i(TAG, "Sent reponse to 'testEvents' command.");
+				break;
+			}
+			}
+		} catch (IOException e) {
+			Log.e(TAG, "IO exception processing custom command.", e);
+		} catch (SiteWhereMessagingException e) {
+			Log.e(TAG, "Messaging exception processing custom command.", e);
+		}
 	}
 
 	/**
